83a7a4cd8218acc77e590a3202582fe06a4a4e6b
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2024-02-22 16:40:37 -0300

    alterando e ajustes no style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,87 @@
-const btnCriptografar = document.querySelector('.btn-cript');
-const btnDescriptografar = document.querySelector('.btn-descript');
-const btnCopiar = document.querySelector('.btn-copiar');
-const primeiroTexto = document.querySelector('.primeiro-texto');
-const textoResult = document.querySelector('.txt-result');
 
-btnCriptografar.addEventListener('click', () => {
+  const btnCriptografar = document.querySelector(".btn-cript");
+  const btnDescriptografar = document.querySelector(".btn-descript");
+  const btnCopiar = document.querySelector(".btn-copiar");
+  const primeiroTexto = document.querySelector(".primeiro-texto");
+  const textoResult = document.querySelector(".txt-result");
+
+  btnCriptografar.addEventListener("click", () => {
     // Verifica se há texto antes de criptografar
-    if (primeiroTexto.value !== '') {
-        criptografar();
-        mostraResultado();
+    if (primeiroTexto.value !== "") {
+      criptografar();
+      mostraResultado();
     }
-});
+  });
 
-btnDescriptografar.addEventListener('click', () => {
+  btnDescriptografar.addEventListener("click", () => {
     descriptografar();
-});
+  });
 
-btnCopiar.addEventListener('click', () => {
+  btnCopiar.addEventListener("click", () => {
     copy();
-});
+  });
 
-primeiroTexto.addEventListener('input', () => {
+  primeiroTexto.addEventListener("input", () => {
     // Converte o texto para minúsculas
     primeiroTexto.value = primeiroTexto.value.toLowerCase();
 
     // Esconde o resultado se o campo de texto estiver vazio
     if (!primeiroTexto.value) {
-        esconderResultado();
+      esconderResultado();
     }
-});
+  });
 
-// Função para criptografar o texto inserido
-const criptografar = () => {
-    textoResult.textContent = substituirTexto(primeiroTexto.value, [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']]);
-};
+  // Função para criptografar o texto inserido
+  const criptografar = () => {
+    textoResult.textContent = substituirTexto(primeiroTexto.value, [
+      ["e", "enter"],
+      ["i", "imes"],
+      ["a", "ai"],
+      ["o", "ober"],
+      ["u", "ufat"],
+    ]);
+  };
 
-// Função para descriptografar o texto inserido
-const descriptografar = () => {
-    textoResult.textContent = substituirTexto(textoResult.value, [['ufat', 'u'], ['ober', 'o'], ['ai', 'a'], ['imes', 'i'], ['enter', 'e']]);
-};
+  // Função para descriptografar o texto inserido
+  const descriptografar = () => {
+    textoResult.textContent = substituirTexto(textoResult.value, [
+      ["ufat", "u"],
+      ["ober", "o"],
+      ["ai", "a"],
+      ["imes", "i"],
+      ["enter", "e"],
+    ]);
+  };
 
-// Função para mostrar o resultado
-const mostraResultado = () => {
-    const containerImg = document.querySelector('.container-resultado');
-    const containerResult = document.querySelector('.container-result-cript');
-    containerImg.style.display = 'none';
-    containerResult.style.display = 'flex';
-};
+  // Função para mostrar o resultado
+  const mostraResultado = () => {
+    const containerImg = document.querySelector(".container-resultado");
+    const containerResult = document.querySelector(".container-result-cript");
+    containerImg.style.display = "none";
+    containerResult.style.display = "flex";
+  };
 
-// Função para esconder o resultado
-const esconderResultado = () => {
-    const containerImg = document.querySelector('.container-resultado');
-    const containerResult = document.querySelector('.container-result-cript');
-    containerImg.style.display = 'flex';
-    containerResult.style.display = 'none';
-};
+  // Função para esconder o resultado
+  const esconderResultado = () => {
+    const containerImg = document.querySelector(".container-resultado");
+    const containerResult = document.querySelector(".container-result-cript");
+    containerImg.style.display = "flex";
+    containerResult.style.display = "none";
+  };
 
-// Função para copiar o texto resultante para a área de transferência
-const copy = () => {
+  // Função para copiar o texto resultante para a área de transferência
+  const copy = () => {
     textoResult.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     alert(`Copiado com sucesso !!!\n"${textoResult.textContent}"`);
-};
+  };
 
-// Função para substituir texto com base em uma lista de substituições
-const substituirTexto = (original, substituicoes) => {
+  // Função para substituir texto com base em uma lista de substituições
+  const substituirTexto = (original, substituicoes) => {
     for (const [padrao, substituto] of substituicoes) {
-        original = original.replace(new RegExp(padrao, 'g'), substituto);
+      original = original.replace(new RegExp(padrao, "g"), substituto);
     }
     return original;
-};
+  };
+
+  
